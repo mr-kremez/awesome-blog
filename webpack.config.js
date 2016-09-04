2ee3423840e97f3c758f1d41b391cbbf020ee3ba
Maks Kremez <mr.kremez@gmail.com>
2016-09-04 20:12:39 +0300

    Add react-hot-reload

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: 'babel'
+            loaders: ['react-hot-loader/webpack', 'babel']
         }]
     },
     resolve: {
