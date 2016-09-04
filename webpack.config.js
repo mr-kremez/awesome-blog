d877476f355e7ca356ee01f05228ada0356012e8
Maks Kremez <mr.kremez@gmail.com>
2016-09-04 15:50:35 +0300

    Add hot reloading. Enhance webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,40 @@
+const webpack = require('webpack');
+
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const ROOT_PATH = path.resolve(__dirname);
+
 module.exports = {
     entry: [
-        './app/src/index.js'
+        path.resolve(ROOT_PATH, 'app/src/index')
     ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     output: {
-        path: __dirname + '/app/build',
+        path: path.resolve(ROOT_PATH, 'app/build'),
         publicPath: '/',
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './app/build'
-    }
+        contentBase: path.resolve(ROOT_PATH, 'app/build'),
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlwebpackPlugin({
+            title: 'Awesome Blog'
+        })
+    ]
 };
